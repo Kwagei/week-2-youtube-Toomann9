cb207c03384cc778bb2c7613a1afd89df5c18e90
Toomann9 <ntoomann9@gmail.com>
2022-11-02 00:22:29 +0000

    love flower

---
+++ youtube.js
@@ -0,0 +1,21 @@
+var menuIcon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".side-bar");
+
+
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+}
+
+var abochiPic = document.querySelector(".abochi");
+var alonZo = document.querySelector(".alonzo-pic");
+var Bisa = document.querySelector(".bisa-kdei");
+var DMW = document.querySelector(".one-dollar");
+var congrats = document.querySelector(".ada-buchi");
+var vengeance = document.querySelector(".minister-guc");
+var omemma = document.querySelector(".judikay");
+var spirit = document.querySelector(".tasha-kiera");
+
+
+abochiPic.onclick = function(){
+    abochi-vid.classList.play("abochi-vid");
+}
